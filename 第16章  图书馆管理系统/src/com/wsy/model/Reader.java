645372bd2b9d567cbba219382398a8e7c195ffe8
haoyualways <71204957+haoyualways@users.noreply.github.com>
2020-10-24 22:49:12 +0800

    1.0
    第一次

---
+++ 第16章  图书馆管理系统/src/com/wsy/model/Reader.java
@@ -0,0 +1,97 @@
+package com.wsy.model;
+
+import java.sql.Date;
+
+public class Reader {
+	private String name;
+	private String sex;
+	private String age;
+	private String identityCard;
+	private String date;
+	private String maxNum;
+	private String tel;
+	private Double keepMoney;
+	private int zj;
+	private String zy;
+	private String ISBN;
+	private String bztime;
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getBztime() {
+		return bztime;
+	}
+
+	public void setBztime(String bztime) {
+		this.bztime = bztime;
+	}
+
+	public String getIdentityCard() {
+		return identityCard;
+	}
+	public void setIdentityCard(String identityCard) {
+		this.identityCard = identityCard;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String isbn) {
+		ISBN = isbn;
+	}
+
+	public Double getKeepMoney() {
+		return keepMoney;
+	}
+	public void setKeepMoney(Double keepMoney) {
+		this.keepMoney = keepMoney;
+	}
+	public String getMaxNum() {
+		return maxNum;
+	}
+	public void setMaxNum(String maxNum) {
+		this.maxNum = maxNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public int getZj() {
+		return zj;
+	}
+	public void setZj(int zj) {
+		this.zj = zj;
+	}
+	public String getZy() {
+		return zy;
+	}
+	public void setZy(String zy) {
+		this.zy = zy;
+	}
+}
